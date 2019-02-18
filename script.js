b890f96ff1bb17e0cfe502be509573a5837f54d5
Radu Cozmi <37863534+raducozmi@users.noreply.github.com>
2019-02-18 11:52:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const hexNumber = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+const hexBtn = document.querySelector('.hexBtn');
+const bodyBcg = document.querySelector('body');
+const hex = document.querySelector('.hex');
+
+hexBtn.addEventListener('click',getHex);
+
+function getHex (){
+    let hexCol = '#';
+    for(let i = 0; i<6; i++){
+        let random = Math.floor(Math.random()*hexNumber.length);
+        //console.log(random);
+        hexCol +=hexNumber[random];
+        //console.log(hexCol);
+      
+    }
+    bodyBcg.style.backgroundColor = hexCol;
+    hex.innerHTML = hexCol;
+}
